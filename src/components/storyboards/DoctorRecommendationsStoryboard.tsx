40dce6de44427de7ab7965427ba6d1c3e8f545a6
vipulghadi <vipulvijayghadi@gmail.com>
2025-03-28 12:29:27 +0530

    deleted existing code

--- src/components/storyboards/DoctorRecommendationsStoryboard.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DoctorRecommendations from "../doctor/DoctorRecommendations";
-
-const DoctorRecommendationsStoryboard = () => {
-  return <DoctorRecommendations />;
-};
-
-export default DoctorRecommendationsStoryboard;
--- src/components/storyboards/SettingsStoryboard.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SettingsPage from "../settings/SettingsPage";
-
-const SettingsStoryboard = () => {
-  return <SettingsPage />;
-};
-
-export default SettingsStoryboard;
